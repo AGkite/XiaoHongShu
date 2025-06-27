f07ae1e407031237e8b25e3827b579451b9511e7
AGkite <liuwenhao8888@gmail.com>
2025-06-27 15:06:37 +0800

    refactor: 更改项目路径

--- xiaohongshu-springcloud/xiaohongshu-auth/src/test/java/com/newone/xiaohongshu/auth/XiaohongshuAuthApplicationTests.java
+++ xiaohongshu-springcloud/xiaohongshu-auth/src/test/java/com/newone/xiaohongshu/auth/XiaohongshuAuthApplicationTests.java
@@ -1,8 +1,10 @@
 package com.newone.xiaohongshu.auth;
 
+import com.alibaba.druid.filter.config.ConfigTools;
 import com.newone.xiaohongshu.auth.domain.dataobject.UserDO;
 import com.newone.xiaohongshu.auth.domain.mapper.UserDOMapper;
 import jakarta.annotation.Resource;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,5 +31,33 @@ class XiaohongshuAuthApplicationTests {
     @Test
     void testDelete() {
         userDOMapper.deleteByPrimaryKey(1L);
+    }
+
+    @Test
+    void testSelect() {
+        userDOMapper.selectByPrimaryKey(2L);
+    }
+
+    @SneakyThrows
+    @Test
+    void testEncrypt() {
+        String password = "123456";
+        String[] keyPair = ConfigTools.genKeyPair(512);
+
+        String privateKey = keyPair[0];
+        String publicKey = keyPair[1];
+
+        String encryptPassword = ConfigTools.encrypt(privateKey, password);
+
+        String decryptPassword = ConfigTools.decrypt(publicKey, encryptPassword);
+
+        log.info(String.format("""
+                
+                privateKey=%s
+                publicKey=%s
+                encryptPassword=%s
+                decryptPassword=%s
+                """, privateKey, publicKey, encryptPassword, decryptPassword));
+
     }
 }
